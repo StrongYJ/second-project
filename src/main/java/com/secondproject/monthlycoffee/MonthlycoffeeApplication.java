545f782a3fe6ba23b0edc1bb85cd5f161f456bd9
StrongYJ <206freeguy@gmail.com>
2023-02-08 16:50:12 +0900

    first commit

---
+++ src/main/java/com/secondproject/monthlycoffee/MonthlycoffeeApplication.java
@@ -0,0 +1,13 @@
+package com.secondproject.monthlycoffee;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonthlycoffeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonthlycoffeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/secondproject/monthlycoffee/MonthlycoffeeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.secondproject.monthlycoffee;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonthlycoffeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
