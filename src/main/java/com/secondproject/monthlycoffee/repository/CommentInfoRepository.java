90983869cb544ce8ba59b06ed3dcac9c82877cd9
doyouee <aquariu92s@naver.com>
2023-02-09 16:36:51 +0900

    230209_이도영

---
+++ src/main/java/com/secondproject/monthlycoffee/repository/CommentInfoRepository.java
@@ -0,0 +1,9 @@
+package com.secondproject.monthlycoffee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.secondproject.monthlycoffee.entity.CommentInfo;
+
+public interface CommentInfoRepository extends JpaRepository<CommentInfo, Long> {
+    
+}
---
+++ src/main/java/com/secondproject/monthlycoffee/repository/ExpenseImageInfoRepository.java
@@ -0,0 +1,9 @@
+package com.secondproject.monthlycoffee.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.secondproject.monthlycoffee.entity.ExpenseImageInfo;
+
+public interface ExpenseImageInfoRepository extends JpaRepository<ExpenseImageInfo, Long> {
+    
+}
