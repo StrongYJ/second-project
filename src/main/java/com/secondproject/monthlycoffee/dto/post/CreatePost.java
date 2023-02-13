bcb2fcb86158a7aa63f86b673f1c18ac90db2715
StrongYJ <206freeguy@gmail.com>
2023-02-13 18:15:32 +0900

    CreatePost error fixed

--- src/main/java/com/secondproject/monthlycoffee/dto/post/CreatePost.java
+++ src/main/java/com/secondproject/monthlycoffee/dto/post/CreatePost.java
@@ -4,7 +4,9 @@ import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 
 public record CreatePost(
-    @Schema(description = " 게시글 등록할 내용")
+    @Schema(description = "게시글로 등록할 지출 식별 번호")
+    Long expenseId,
+    @Schema(description = "게시글 등록할 내용")
     @NotBlank
     String content
 ) {
