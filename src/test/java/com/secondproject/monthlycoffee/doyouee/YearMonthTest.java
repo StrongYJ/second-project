4e23c70fa29b2a86222eceeb20a7cfe9458321a5
doyouee <aquariu92s@naver.com>
2023-02-15 18:09:02 +0900

    230215

--- src/test/java/com/secondproject/monthlycoffee/doyouee/YearMonthTest.java
+++ src/test/java/com/secondproject/monthlycoffee/doyouee/YearMonthTest.java
@@ -5,16 +5,22 @@ import java.time.YearMonth;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.secondproject.monthlycoffee.entity.IncomeInfo;
+
+import jakarta.validation.groups.ConvertGroup.List;
+import lombok.AllArgsConstructor;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @SpringBootTest
 @Slf4j
 public class YearMonthTest {
     
+    
+    
     @Test
     void yearMonth() {
         log.info("year Month = {}", YearMonth.now());
     }
-
 
 }
