170a130ebc847450af37f39af7da707647abbe20
StrongYJ <206freeguy@gmail.com>
2023-02-16 14:26:24 +0900

    20230216

--- src/main/java/com/secondproject/monthlycoffee/repository/PostInfoRepository.java
+++ src/main/java/com/secondproject/monthlycoffee/repository/PostInfoRepository.java
@@ -1,5 +1,11 @@
 package com.secondproject.monthlycoffee.repository;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.secondproject.monthlycoffee.entity.ExpenseInfo;
@@ -7,5 +13,16 @@ import com.secondproject.monthlycoffee.entity.PostInfo;
 
 public interface PostInfoRepository extends JpaRepository<PostInfo, Long> {
     boolean existsByExpense(ExpenseInfo expense);
-    
+
+    @Override
+    @EntityGraph(attributePaths = {"expense"})
+    Optional<PostInfo> findById(Long id);
+
+    @Override
+    @EntityGraph(attributePaths = {"expense"})
+    List<PostInfo> findAll();
+
+    @Override
+    @EntityGraph(attributePaths = {"expense"})
+    Page<PostInfo> findAll(Pageable pageable);
 }
