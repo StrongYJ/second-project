82c1811f2564e5dcf8b9cf339eb66a0bd71e59e1
StrongYJ <206freeguy@gmail.com>
2023-02-17 20:33:32 +0900

    20230217

---
+++ src/test/java/com/secondproject/monthlycoffee/strongyj/RedisAccessTokenCrdTest.java
@@ -0,0 +1,53 @@
+package com.secondproject.monthlycoffee.strongyj;
+
+import java.util.UUID;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.secondproject.monthlycoffee.token.AccessTokenBlackList;
+import com.secondproject.monthlycoffee.token.AccessTokenBlackListRepository;
+
+@DataRedisTest
+@ActiveProfiles("test")
+public class RedisAccessTokenCrdTest {
+
+    @Autowired private AccessTokenBlackListRepository access;
+
+    @Test
+    void create() {
+        AccessTokenBlackList token = new AccessTokenBlackList("fwnfjwenfkwnfwknefkjnj", 1000L);
+        access.save(token);
+    }
+
+    @Test
+    void read() {
+        AccessTokenBlackList accessTokenBlackList = new AccessTokenBlackList(UUID.randomUUID().toString(), (1000l * 60 * 5));
+        access.save(accessTokenBlackList);
+
+        AccessTokenBlackList accessTokenBlackList2 = access.findById(accessTokenBlackList.getToken()).get();
+        Assertions.assertThat(accessTokenBlackList.getToken()).isEqualTo(accessTokenBlackList2.getToken());
+    }
+
+    @Test
+    void delete() {
+        AccessTokenBlackList accessTokenBlackList = new AccessTokenBlackList(UUID.randomUUID().toString(), (1000l * 60 * 5));
+        access.save(accessTokenBlackList);
+
+        access.deleteById(accessTokenBlackList.getToken());
+    }
+
+    @Test
+    void 만료() throws InterruptedException {
+        AccessTokenBlackList accessTokenBlackList = new AccessTokenBlackList(UUID.randomUUID().toString(), 1000L);
+        access.save(accessTokenBlackList);
+
+        Thread.sleep(1000L);
+        
+        Assertions.assertThat(access.findById(accessTokenBlackList.getToken()).isPresent()).isEqualTo(false);
+        
+    }
+}
---
+++ src/test/java/com/secondproject/monthlycoffee/strongyj/RedisRefreshTokenCrdTest.java
@@ -0,0 +1,53 @@
+package com.secondproject.monthlycoffee.strongyj;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.UUID;
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.data.redis.DataRedisTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.secondproject.monthlycoffee.token.RefreshToken;
+import com.secondproject.monthlycoffee.token.RefreshTokenRepository;
+
+@DataRedisTest
+@ActiveProfiles("test")
+public class RedisRefreshTokenCrdTest {
+
+    @Autowired private RefreshTokenRepository refreshRepo;
+    private final long REFRESH_EXPIRATION_TIME = 1000L;
+
+    @Test
+    void create() {
+        refreshRepo.save(new RefreshToken(UUID.randomUUID().toString(), 1L, REFRESH_EXPIRATION_TIME));
+    }
+
+    @Test
+    void read() {
+        RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), 1L, 
+                REFRESH_EXPIRATION_TIME);
+        refreshRepo.save(refreshToken);
+
+        RefreshToken refreshToken2 = refreshRepo.findById(refreshToken.getToken()).get();
+
+        assertThat(refreshToken.getToken()).isEqualTo(refreshToken2.getToken());
+        assertThat(refreshToken.getMemberId()).isEqualTo(refreshToken2.getMemberId());
+    }
+
+    private final CountDownLatch waiter = new CountDownLatch(1);
+    
+    @Test
+    void delete() throws InterruptedException {
+        RefreshToken refreshToken = new RefreshToken(UUID.randomUUID().toString(), 1L, REFRESH_EXPIRATION_TIME);
+        refreshRepo.save(refreshToken);
+        
+        waiter.await(REFRESH_EXPIRATION_TIME + 1l, TimeUnit.MILLISECONDS);
+    
+        assertThat(refreshRepo.findById(refreshToken.getToken()).isPresent()).isFalse();
+    }
+    
+}
