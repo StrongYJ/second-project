661335db72d906e7ef4d134947981f51ca11b2ca
affkdy <affkdy@gmail.com>
2023-02-21 14:33:29 +0900

    지출 토큰 적용

--- src/main/java/com/secondproject/monthlycoffee/api/ExpenseAPIController.java
+++ src/main/java/com/secondproject/monthlycoffee/api/ExpenseAPIController.java
@@ -2,6 +2,8 @@ package com.secondproject.monthlycoffee.api;
 
 import java.util.*;
 
+import com.secondproject.monthlycoffee.config.security.AuthMember;
+import com.secondproject.monthlycoffee.config.security.dto.AuthDto;
 import com.secondproject.monthlycoffee.dto.expense.*;
 
 import com.secondproject.monthlycoffee.entity.type.LikeHate;
@@ -36,73 +38,96 @@ public class ExpenseAPIController {
 
     @Operation(summary = "지출 등록", description = "지출 내용을 등록합니다.")
     @PostMapping("")
-    public ResponseEntity<MessageExpenseDto> putExpense(@Parameter(description = "등록할 내용") @RequestBody ExpenseCreateDto data, @Parameter(description = "회원 번호", example = "1") @RequestParam Long memberId) {
-        return new ResponseEntity<>(eService.putExpense(data ,memberId), HttpStatus.OK);
+    public ResponseEntity<MessageExpenseDto> putExpense(
+            @Parameter(description = "등록할 내용") @RequestBody ExpenseCreateDto data,
+            @AuthMember AuthDto authDto) {
+        return new ResponseEntity<>(eService.putExpense(data, authDto.id()), HttpStatus.OK);
     }
 
     @Operation(summary = "이미지 등록", description = "이미지를 등록합니다.")
-    @PostMapping(value = "/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<MessageExpenseDto> putExpense(@Parameter(description = "등록할 이미지") @RequestPart MultipartFile[] file, @Parameter(description = "게시글 번호", example = "1") @RequestParam Long expenseId) {
+    @PostMapping(value = "/{expense-id}/image", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<MessageExpenseDto> putExpense(
+            @Parameter(description = "등록할 이미지") @RequestPart MultipartFile[] file,
+            @Parameter(description = "게시글 번호", example = "1") @PathVariable("expense-id") Long expenseId) {
         return new ResponseEntity<>(eService.putExpenseImage(file, expenseId), HttpStatus.OK);
     }
 
     @Operation(summary = "회원 지출 조회", description = "회원 식별 번호만 입력시 그 회원의 전체 지출 내역이 조회됩니다. 평가(assessment)값 조회연월값(date)을 같이 넣으면 두조건 모두에 해당하는 지출 내역만 조회합니다. 각각 검색도 가능합니다.")
-    @GetMapping("/{member-id}")
-    public ResponseEntity<List<ExpenseDetailDto>> getExpenseList(@Parameter(description = "회원 식별 번호") @PathVariable("member-id") Long memberId, @Parameter(description = "지출 평가 목록", example = "LIKE") @RequestParam(required = false) LikeHate assessment, @Parameter(description = "년도(23)와 월(02)", example = "2302") Integer date) {
-        return new ResponseEntity<>(eService.getExpense(memberId, assessment, date), HttpStatus.OK);
+    @GetMapping("")
+    public ResponseEntity<List<ExpenseDetailDto>> getExpenseList(
+            @AuthMember AuthDto authDto,
+            @Parameter(description = "지출 평가 목록", example = "LIKE") @RequestParam(required = false) LikeHate assessment,
+            @Parameter(description = "년도(23)와 월(02)", example = "2302") Integer date) {
+        return new ResponseEntity<>(eService.getExpense(authDto.id(), assessment, date), HttpStatus.OK);
     }
 
     @Operation(summary = "회원 총 지출액 조회", description = "회원의 총 지출 액 및 기간별 지출 액을 조회합니다. 시작과 끝 연월을 입력하시면 그 기간의 총액 산출, 시작연월 미입력시 끝 연월 포함 이전 기간 총 지출액, 끝 연월 미입력시 시작 연월 포함 이후 기간 총 지출액, 미입력시 총 지출액 조회입니다.")
-    @GetMapping("/total/{member-id}")
-    public ResponseEntity<TotalExpenseDto> getTotalExpense(@Parameter(description = "회원 식별 번호") @PathVariable("member-id") Long memberId, @Parameter(description = "년도(23)와 월(02)", example = "startDate=2302") @RequestParam(required = false, defaultValue = "") Integer startDate, @Parameter(description = "년도(22)와 월(03)", example = "&endDate=2303") Integer endDate) {
-        return new ResponseEntity<>(eService.getTotalExpense(memberId, startDate, endDate), HttpStatus.OK);
+    @GetMapping("/total")
+    public ResponseEntity<TotalExpenseDto> getTotalExpense(
+            @AuthMember AuthDto authDto,
+            @Parameter(description = "년도(23)와 월(02)", example = "startDate=2302") @RequestParam(required = false, defaultValue = "") Integer startDate,
+            @Parameter(description = "년도(22)와 월(03)", example = "&endDate=2303") Integer endDate) {
+        return new ResponseEntity<>(eService.getTotalExpense(authDto.id(), startDate, endDate), HttpStatus.OK);
     }
 
     @Operation(summary = "지출 수정", description = "지출 식별 번호를 통해 등록된 지출을 수정합니다.")
     @PatchMapping("/{expense-id}")
-    public ResponseEntity<MessageExpenseDto> updateExpense(@Parameter(description = "지출 식별 번호", example = "1") @PathVariable("expense-id") Long expenseNo, @RequestBody ExpenseDetailDto data) {
+    public ResponseEntity<MessageExpenseDto> updateExpense(
+            @Parameter(description = "지출 식별 번호", example = "1") @PathVariable("expense-id") Long expenseNo,
+            @Parameter(description = "수정을 원하는 데이터") @RequestBody ExpenseDetailDto data) {
         return new ResponseEntity<>(eService.update(expenseNo, data), HttpStatus.OK);
     }
 
     @Operation(summary = "지출 삭제", description = "지출 식별 번호를 통해 등록된 지출을 삭제합니다. 삭제시 연결되어있는 이미지가 같이 삭제됩니다.")
     @DeleteMapping("/{expense-id}")
-    public ResponseEntity<MessageExpenseDto> deleteExpense(@Parameter(description = "지출 식별 번호", example = "1") @PathVariable("expense-id") Long id) {
+    public ResponseEntity<MessageExpenseDto> deleteExpense(
+            @Parameter(description = "지출 식별 번호", example = "1") @PathVariable("expense-id") Long id) {
         return new ResponseEntity<>(eService.delete(id), HttpStatus.OK);
     }
 
     @Operation(summary = "이미지 다운로드", description = "이미지를 다운로드할 수 있는 주소입니다.")
     @GetMapping("/image/{filename}")
-    public ResponseEntity<Resource> getImage (@Parameter(description = "이미지 이름") @PathVariable String filename, HttpServletRequest request) throws Exception {
+    public ResponseEntity<Resource> getImage (
+            @Parameter(description = "이미지 이름") @PathVariable String filename,
+            HttpServletRequest request) throws Exception {
         return eService.getImage(filename, request);
     }
 
     @Operation(summary = "이미지 삭제", description = "이미지 식별 번호를 통해 등록된 이미지를 삭제합니다.")
     @DeleteMapping("/image/{image-id}")
-    public ResponseEntity<MessageExpenseDto> deleteImage(@Parameter(description = "이미지 식별 번호", example = "1") @PathVariable("image-id") Long id) {
+    public ResponseEntity<MessageExpenseDto> deleteImage(
+            @Parameter(description = "이미지 식별 번호", example = "1") @PathVariable("image-id") Long id) {
         return new ResponseEntity<>(eService.deleteImage(id), HttpStatus.OK);
     }
 
     @Operation(summary = "더미 제작", description = "원하는 회원의 더미를 size 개 생성합니다.")
     @PostMapping("/dummy")
-    public ResponseEntity<MessageExpenseDto> putDummy(@RequestParam Long userNo, Integer size) {
+    public ResponseEntity<MessageExpenseDto> putDummy(
+            @RequestParam Long userNo, Integer size) {
         return new ResponseEntity<>(eService.dummyData(userNo, size), HttpStatus.OK);
     }
 
     @Operation(summary = "카테고리 조회", description = "회원의 카테고리를 검색합니다.")
-    @GetMapping("/category/{member-id}")
-    public ResponseEntity<List<ExpenseDetailDto>> getCategory(@Parameter(description = "회원 식별 번호", example = "1") @PathVariable("member-id") Long memberId, @Parameter(description = "조회연월", example = "2302") @RequestParam Integer date, @Parameter(description = "카테고리", example = "아메리카노") String keyword) {
-        return new ResponseEntity<>(eService.getCategory(date, keyword, memberId), HttpStatus.OK);
+    @GetMapping("/category")
+    public ResponseEntity<List<ExpenseDetailDto>> getCategory(
+            @AuthMember AuthDto authDto,
+            @Parameter(description = "조회연월", example = "2302") @RequestParam Integer date,
+            @Parameter(description = "카테고리", example = "아메리카노") String keyword) {
+        return new ResponseEntity<>(eService.getCategory(date, keyword, authDto.id()), HttpStatus.OK);
     }
 
     @Operation(summary = "브랜드 조회", description = "회원의 브랜드를 검색합니다.")
-    @GetMapping("/brand/{member-id}")
-    public ResponseEntity<List<ExpenseDetailDto>> getBrand(@Parameter(description = "회원 식별 번호", example = "1") @PathVariable("member-id") Long memberId, @Parameter(description = "조회연월", example = "2302") @RequestParam Integer date, @Parameter(description = "브랜드", example = "스타벅스") String keyword) {
-        return new ResponseEntity<>(eService.getBrand(date, keyword, memberId), HttpStatus.OK);
+    @GetMapping("/brand")
+    public ResponseEntity<List<ExpenseDetailDto>> getBrand(
+            @AuthMember AuthDto authDto,
+            @Parameter(description = "조회연월", example = "2302") @RequestParam Integer date,
+            @Parameter(description = "브랜드", example = "스타벅스") String keyword) {
+        return new ResponseEntity<>(eService.getBrand(date, keyword, authDto.id()), HttpStatus.OK);
     }
 
     @Operation(summary = "사용자의 취향 조회", description = "좋아요를 누른 내용을 토대로 회원의 취향을 추정합니다.")
-    @GetMapping("/like/{member-id}")
-    public ResponseEntity<MessageExpenseDto> getBrand(@Parameter(description = "회원 식별 번호", example = "1") @PathVariable("member-id") Long memberId) {
-        return new ResponseEntity<>(eService.likeStyle(memberId), HttpStatus.OK);
+    @GetMapping("/preference")
+    public ResponseEntity<MessageExpenseDto> getBrand(@AuthMember AuthDto authDto) {
+        return new ResponseEntity<>(eService.likeStyle(authDto.id()), HttpStatus.OK);
     }
 }
